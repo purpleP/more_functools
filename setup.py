a136c344e33878dbde63afb3c0232fbb6ba1992e
Michael Doronin <warrior2031@gmail.com>
2016-07-22 15:08:34 +0300

    removed unused install_requires from setup.py

--- setup.py
+++ setup.py
@@ -4,14 +4,8 @@ from distutils.core import setup
 setup(
     name='more_functools',
     version='0.1',
-    install_requires=[
-        'sqlalchemy',
-        'MySQL-python',
-        'cached_property',
-        'backports.functools_lru_cache',
-    ],
     author='michael',
     py_modules=['more_functools'],
     author_email='warrior2031@mail.ru',
-    description='sqlalchemy based models for ISS'
+    description='sqlalchemy based models for ISS',
 )
